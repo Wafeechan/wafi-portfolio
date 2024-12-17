fa58ce9e82ed190564c34fb810357c1892ccfe81
Wafeechan <awn.1905@gmail.com>
2024-12-17 21:17:50 +0800

    Update dependancies

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/wafi-portfolio",
+  base: "/wafi-portfolio/",
   server: {
     historyApiFallback: true, // Allows SPA to handle routing properly
   },
